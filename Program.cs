ae3cb2c355bde62e2d742e602f331ba6c7f76a35
Rui Inácio <146144535+ruiinacio1982@users.noreply.github.com>
2023-10-15 20:22:03 +0100

    Acrescentar código para testar

--- Program.cs
+++ Program.cs
@@ -13,6 +13,12 @@ namespace Calculos
             Console.WriteLine("Aplicação Cálculos");
             Console.WriteLine($"4 + 2 = {Aritmetica.Somar(4,2)}");
             Console.WriteLine($"4 - 2 = {Aritmetica.Subtrair(4, 2)}");
+
+            double temp1 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit, 36);
+            double temp2 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.FahrenheitCelsisus, 100);
+
+            Console.WriteLine($"36ºC = {temp1}º Fahrneight");
+            Console.WriteLine($"100ºF = {temp2}º Celsius");
             Console.ReadKey();
         }
     }
